a5a2c4263041dfae451ee431125676f1a239000f
sujalpoudel <41342242+sujalpoudel@users.noreply.github.com>
2023-08-22 11:26:19 +1000

    Login page

--- index.php
+++ index.php
@@ -1,10 +1,13 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title></title>
+<title>Job Portal Website</title>
+<meta name="description" content="Best Job Portal Website in Sydney">
+<meta name="keywords" content="Job Portal">
+
 </head>
 <body>
-	<a href="job_post.php">job post</a>
+	<!-- <a href="job_post.php">job post</a>
 <a href="login.html">login</a>
 <a href="index.php">index</a>
 
@@ -16,7 +19,7 @@
 	} else {
 	    echo "You are not logged in.";
 	}
-   ?>
+   ?>  -->
 
 
 	
